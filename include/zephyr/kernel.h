406d647fcf1b51ad8e05b15a4c233b4f99a7f25c
Peter Mitsis <peter.mitsis@intel.com>
2023-05-31 14:26:50 -0400

    kernel: FIFO and LIFO have their own sections
    Instead of placing both FIFOs and LIFOs into a common k_queue
    type section, they are now placed into their own custom iterable
    secitons. This is necessary when employing object cores as FIFOs
    and LIFOs will be associated with different lists.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- include/zephyr/kernel.h
+++ include/zephyr/kernel.h
@@ -2492,7 +2492,7 @@ struct k_fifo {
  * @param name Name of the FIFO queue.
  */
 #define K_FIFO_DEFINE(name) \
-	STRUCT_SECTION_ITERABLE_ALTERNATE(k_queue, k_fifo, name) = \
+	STRUCT_SECTION_ITERABLE(k_fifo, name) = \
 		Z_FIFO_INITIALIZER(name)
 
 /** @} */
@@ -2612,7 +2612,7 @@ struct k_lifo {
  * @param name Name of the fifo.
  */
 #define K_LIFO_DEFINE(name) \
-	STRUCT_SECTION_ITERABLE_ALTERNATE(k_queue, k_lifo, name) = \
+	STRUCT_SECTION_ITERABLE(k_lifo, name) = \
 		Z_LIFO_INITIALIZER(name)
 
 /** @} */
